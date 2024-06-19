4486075a21ffdcd5195ebf114ac20b41a460a3b5
Neeru Kapoor <467neeru@gmail.com>
2024-06-19 23:16:42 +0530

    frontend scaffolding done

--- frontend/chat application/src/components/messages/MessageContainer.tsx
+++ frontend/chat application/src/components/messages/MessageContainer.tsx
@@ -1,8 +1,13 @@
+import { TiMessage } from "react-icons/ti";
+import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
 const MessageContainer = () => {
+    const noChatSelected = false;
     return(
-        <div className="min-w-[450px] h-[33rem] flex flex-col overflow-auto">
+        <>
+        {noChatSelected ? (<NoChatSelected/>) : (
+            <div className="min-w-[450px] h-[33rem] flex flex-col overflow-auto">
             <>
                 <div className="bg-slate-500 px-4 py-2 mb-2">
                     <span className="label-text">To:</span>{" "}
@@ -10,10 +15,24 @@ const MessageContainer = () => {
                 </div>
                 
                 <Messages/>
-                {/* <MessageInput/> */}
+                <MessageInput/>
             </>
         </div>
+        )}
+        </>
     )
 }
 
 export default MessageContainer;
+
+const NoChatSelected = () => {
+    return(
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="px-4 text-center font-semibold sm:text-lg md:text-xl flex flex-col items-center gap-2">
+                <p>Welcome Jon 👋</p>
+                <p>Select a Chat to Start Messaging</p>
+                <TiMessage className="text-3xl md:text-6xl"/>
+            </div>
+        </div>
+    )
+}
